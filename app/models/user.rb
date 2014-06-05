d0b8e81a679aa8ace5cd49a3c10ec6220dd4bc93
davidhuff2014 <David.Huff@computer-critters.com>
2014-06-05 08:03:39 -0400

    make sure to turn off everything before commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 # encoding: UTF-8
+
+# setting up relationship to posts
 class User < ActiveRecord::Base
+  has_many :posts
+  has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140604165448) do
+ActiveRecord::Schema.define(version: 20140605025408) do
+
+  create_table "comments", force: true do |t|
+    t.text     "body"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "posts", force: true do |t|
     t.string   "url"
@@ -19,12 +30,15 @@ ActiveRecord::Schema.define(version: 20140604165448) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
     t.string   "username"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "comment_id"
+    t.integer  "post_id"
   end
 
 end

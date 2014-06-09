d17e695737667eb5ccefa5f17da65f2bba2c6f43
davidhuff2014 <David.Huff@computer-critters.com>
2014-06-09 13:45:47 -0400

    after lunch snap

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,27 @@
 # posts controller
 class PostsController < ApplicationController
   def index
-
+    @posts = Post.all
   end
+
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+
+  end
+
+  def create
+
+  end
+ 
+  def edit
+
+  end
+
+  def update
+
+  end
+  
 end

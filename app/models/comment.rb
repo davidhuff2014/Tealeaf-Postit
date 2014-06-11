b7cb479097351a38df00299aa48a130951263c16
davidhuff2014 <David.Huff@computer-critters.com>
2014-06-11 15:55:02 -0400

    afternoon snap

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,8 @@
+# encoding: UTF-8
+
 # forms the association to both post and user
 class Comment < ActiveRecord::Base
+  validates :body, presence: true, length: { minimum: 3 }
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   belongs_to :post
 end

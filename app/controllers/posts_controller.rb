1ab4aa6a6dedb64ba129b388b753763661f7c033
davidhuff2014 <David.Huff@computer-critters.com>
2014-06-11 08:08:17 -0400

    commit untracked changes before pull

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,15 +21,23 @@ class PostsController < ApplicationController
       flash[:notice] = 'Your post was created.'
       redirect_to posts_path
     else
-      render 'new'
+      render :new
     end
-    
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      flash[:notice] = 'The post was updated.'
+      redirect_to posts_path
+    else
+      render :edit
+    end
   end
 
   private

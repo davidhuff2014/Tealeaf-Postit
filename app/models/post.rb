cea7391c85b93ceab6dad66250a25db5fb144288
davidhuff2014 <David.Huff@computer-critters.com>
2014-06-11 22:53:12 -0400

    unique to uniqueness

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,6 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
 
   validates :title, presence: true
-  validates :url, presence: true, unique: true
+  validates :url, presence: true, uniqueness: true
   validates :description, presence: true, length: { minimum: 3 }
 end

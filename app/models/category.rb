3fb5c212f5b93e9f20ae9ba94847997c47c13fd3
davidhuff2014 <David.Huff@computer-critters.com>
2014-06-16 11:07:47 -0400

    apply stash

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,7 @@
 
 # setting up category
 class Category < ActiveRecord::Base
+  validates :name, presence: true, length: { minimum: 3 }
   has_many :post_categories
   has_many :posts, through: :post_categories
 end

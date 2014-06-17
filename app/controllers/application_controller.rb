6122a392acda323936dd1eb5cdf0d1885d7246e3
davidhuff2014 <David.Huff@computer-critters.com>
2014-06-17 08:20:27 -0400

    add error form to edit and clean up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
+# encoding: UTF-8
+
+# main application controller with protect_from_forgery
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.

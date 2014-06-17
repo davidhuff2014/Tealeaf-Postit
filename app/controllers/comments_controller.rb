946c5663046735b4191f4d223bef8a6cd9858812
davidhuff2014 <David.Huff@computer-critters.com>
2014-06-17 13:27:23 -0400

    after lunch push, comments not displaying

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,12 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @comment = Comment.new(params.require(:comment).permit(:body))
+    # @post = Post.find(params[:post_id], params[:user_id])
+
+    # @post.creator = User.first  # TODO: only until we have authentication
+    # @comment = Comment.new(params.require(:comment).permit(:body))
+    @comment = @post.comments.build(params.require(:comment).permit(:body))
+    @comment.creator = User.first
 
     if @comment.save
       flash[:notice] = 'Your comment was added.'

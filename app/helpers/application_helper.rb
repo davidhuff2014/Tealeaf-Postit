303a889d4cc259716fcaf70955a6e4409ac88828
davidhuff2014 <David.Huff@computer-critters.com>
2014-06-18 10:28:54 -0400

    after Lesson 2 Lecture 4 comments, categories, check_boxes working

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
+# encoding: UTF-8
+
+# anyting in this helper is available application wide
 module ApplicationHelper
+  def fix_url(str)
+    str.starts_with?('http://') ? str : "http://#{str}"
+  end
 end

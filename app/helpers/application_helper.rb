eaf9e3144e5f5bd77c85c7c79e542d55ed170a71
davidhuff2014 <David.Huff@computer-critters.com>
2014-06-19 15:29:18 -0400

    added datetime helper but preferred my own display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,10 @@ module ApplicationHelper
   def fix_url(str)
     str.starts_with?('http://') ? str : "http://#{str}"
   end
+
+  # this helper isn't utilized I prefer
+  # using to_formatted_s(:long)
+  def display_datetime(dt)
+    dt.strftime('%m/%d/%Y %l:%M%P %Z')
+  end
 end

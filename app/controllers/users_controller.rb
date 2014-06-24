34ce7e8a069bc22d6106c7918208b9d50918ad58
davidhuff2014 <David.Huff@computer-critters.com>
2014-06-24 07:57:26 -0400

    snap after registration with automatic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      flash[:notice] = 'You are registered.'
+      # flash[:notice] = 'You are registered.'
+      session[:user_id] = @user.id
+      flash[:notice] = "Welcome, #{@user.username} you are now registered and logged in!"
       redirect_to root_path
     else
       render :new
